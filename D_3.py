f447e3f162529e866e3111981100ce221ed5b1b6
mkenji0628 <minamotojonisann@yahoo.co.jp>
2022-01-28 17:20:45 +0900

    first commit

---
+++ D_3.py
@@ -0,0 +1,18 @@
+class Square:
+    class Square:
+        def __init__(self, side):
+            self.side = side
+
+        def area(self):
+            return (self.side ** 2)
+
+        def diagonal(self):
+            return ((2 * (self.side ** 2)) ** 0.5)
+
+    square1 = Square(side=1.5)
+    print(square1.area())  # 2.25
+    print(square1.diagonal())  # 2.12
+
+    square2 = Square(side=15)
+    print(square2.area())  # 225
+    print(square2.diagonal())  # 21.21
